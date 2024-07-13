0566461cdc08e006fad738a2ca30134aecc730ca
Anjana Tissera <tisserapac@yahoo.com>
2024-07-13 23:52:06 +0700

    Completed lesson 87

---
+++ 04-investment-app-with-service/src/app/investment-input.model.ts
@@ -0,0 +1,6 @@
+export interface InverstmentInput {
+    initialInvestment: number,
+    duration: number,
+    expectedReturn: number,
+    annualInvestment: number
+}
---
+++ 04-investment-app-with-service/src/app/investment.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, signal } from "@angular/core";
+import { InverstmentInput } from "./investment-input.model";
+
+@Injectable({ providedIn: 'root' })
+export class InvestmentService {
+    // resultData?: {
+    //     year: number,
+    //     interest: number,
+    //     valueEndOfYear: number,
+    //     annualInvestment: number,
+    //     totalInterest: number,
+    //     totalAmountInvested: number,
+    // }[];
+
+    resultData = signal<{
+        year: number,
+        interest: number,
+        valueEndOfYear: number,
+        annualInvestment: number,
+        totalInterest: number,
+        totalAmountInvested: number,
+    }[] | undefined>(undefined);
+
+    calculateInvestmentResults(data: InverstmentInput) {
+        const { initialInvestment, duration, expectedReturn, annualInvestment } = data;
+        const annualData = [];
+        let investmentValue = initialInvestment;
+
+        for (let i = 0; i < duration; i++) {
+            const year = i + 1;
+            const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+            investmentValue += interestEarnedInYear + annualInvestment;
+            const totalInterest =
+                investmentValue - annualInvestment * year - initialInvestment;
+            annualData.push({
+                year: year,
+                interest: interestEarnedInYear,
+                valueEndOfYear: investmentValue,
+                annualInvestment: annualInvestment,
+                totalInterest: totalInterest,
+                totalAmountInvested: initialInvestment + annualInvestment * year,
+            });
+        }
+        this.resultData.set(annualData);
+    }
+}
