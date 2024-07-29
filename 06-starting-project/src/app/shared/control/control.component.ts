bd431496cd88211321abbfd75c4a30c3bd760859
Anjana Tissera <tisserapac@yahoo.com>
2024-07-29 23:40:40 +0700

    Completed lesson 118

--- 06-starting-project/src/app/shared/control/control.component.ts
+++ 06-starting-project/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,8 +6,23 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked on control');
+  // }
   label = input.required<string>();
+
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('Clicked on control');
+    console.log(this.el)
+  }
 }
